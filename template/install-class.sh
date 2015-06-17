10dd5af530e39a49f8cf83040f79dee8ba0eda6b
Andreï Kostyrka <fifis.himik@gmail.com>
2015-06-17 20:59:26 +0200

    Continuing the merge

---
+++ template/install-class.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# sudo mkdir /usr/local/texlive/2014/texmf-dist/tex/latex/epsilonj
+sudo cp epsilonj.sty /usr/local/texlive/2014/texmf-dist/tex/latex/epsilonj/epsilonj.sty
+sudo texhash
+
