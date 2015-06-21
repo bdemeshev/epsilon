0eb9c2b7c759594de4dfacba4a72269ca146c4a1
Boris Demeshev <boris.demeshev@gmail.com>
2015-06-21 14:27:22 +0300

    install class update and pdfs :)

--- e_001/pdfpage.sh
+++ e_001/pdfpage.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# именно в этом порядке статьи будут идти в журнале:
 ARTICLES="functional-form
 metrics-errors
 knn
@@ -9,11 +11,14 @@ qr-code
 season
 problems
 stol_zakazov"
-CUM=3
+CUM=3 # стартовая страница для новой статьи (сейчас для первой)
 for i in $ARTICLES
 do
 cd $i
 echo $CUM > 0pagecount.txt
+
+# batchmode скрывает ошибки, поэтому компиляция может внешне пройти без ошибок, а pdf не обновлён
+# если что, надо смотреть log файлы
 pdflatex -interaction batchmode $i
 pdflatex -interaction batchmode  $i
 biber $i
@@ -26,4 +31,6 @@ CUM=$(($CUM + $INCR))
 echo "Next document starts with page: $CUM"
 cd ../
 done
+
+# компилируем журнал целиком
 pdflatex -interaction batchmode e_001.tex
--- template/install-class.sh
+++ template/install-class.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
-# sudo mkdir /usr/local/texlive/2014/texmf-dist/tex/latex/epsilonj
-sudo cp epsilonj.sty /usr/local/texlive/2014/texmf-dist/tex/latex/epsilonj/epsilonj.sty
-sudo texhash
 
+# узнаем, куда пользователю надо ставить стилевые файлы
+WTEX=$(kpsewhich -var-value=TEXMFHOME)
+
+# подложим ему туда кукушонка эпсилон
+sudo mkdir $WTEX/tex/latex/epsilonj
+sudo cp epsilonj.sty $WTEX/tex/latex/epsilonj/epsilonj.sty
+
+# sudo mkdir /usr/local/texlive/2014/texmf-dist/tex/latex/epsilonj
+# sudo cp epsilonj.sty /usr/local/texlive/2014/texmf-dist/tex/latex/epsilonj/epsilonj.sty
+
+sudo texhash # в общем-то не нужен, так как мы кукушонка правильно кладём, но пусть будет
+
+# создано по мотивам
+# http://tex.stackexchange.com/questions/1137
